a26d166d2b9e0421ac2cabb627a512b5d6682b1f
Uğur Ünsal <ugurunsalitu34@gmail.com>
2025-01-06 17:37:53 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,35 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+        return barking && (hourOfDay < 8 || hourOfDay > 20);
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+    public static boolean hasTeen(int age1, int age2, int age3) {
+        return (age1 >= 13 && age1 <= 19) || 
+               (age2 >= 13 && age2 <= 19) || 
+               (age3 >= 13 && age3 <= 19);
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int upperLimit = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= upperLimit;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
