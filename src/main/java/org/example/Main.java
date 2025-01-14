bbe3be3dc7eeafafcad9af893d7fb0626aac94d3
Uğur Ünsal <ugurunsalitu34@gmail.com>
2025-01-15 02:28:43 +0300

    last

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,9 +13,9 @@ public class Main {
     }
 
     public static boolean hasTeen(int age1, int age2, int age3) {
-        return (age1 >= 13 && age1 <= 19) || 
-               (age2 >= 13 && age2 <= 19) || 
-               (age3 >= 13 && age3 <= 19);
+        return (age1 >= 13 && age1 <= 19) ||
+                (age2 >= 13 && age2 <= 19) ||
+                (age3 >= 13 && age3 <= 19);
     }
 
     public static boolean isCatPlaying(boolean summer, int temperature) {
